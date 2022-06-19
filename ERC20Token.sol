eb25d56ecb70793f4281da58c0a80d41659b40e6
cryptichic <syberchief@gmail.com>
2022-06-19 17:09:30 -0500

    adding constructor functionality

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -2,3 +2,12 @@
 pragma solidity ^0.8.4;
 
 // creating an ERC20 token contract
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ERC20Token is ERC20 {
+
+    constructor(uint256 initialSupply) ERC20("Lady", "LADY") {
+        _mint(msg.sender, initialSupply);
+    }
+}
