3902a2596aead1ab011ed439d48655bcd49593c8
cryptichic <syberchief@gmail.com>
2022-06-21 19:42:47 -0500

    adding state variables

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -6,8 +6,14 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ERC20Token is ERC20 {
-
-    constructor(uint256 initialSupply) ERC20("Lady", "LADY") {
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name = "Lady";
+    string public symbol = "LDY";
+    uint8 public decimals = 18;
+    
+    constructor(uint256 initialSupply) ERC20("Lady", "LDY") {
         _mint(msg.sender, initialSupply);
     }
 }
