4ba93d6af7648315dd2ab7ebabbbc0fecc03db58
cryptichic <syberchief@gmail.com>
2022-07-05 18:19:54 -0500

    adding mint functionality and burn functionality

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.5;
 
 // creating a standard ERC20 token contract
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Token is ERC20 {
+    address public admin;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
@@ -15,5 +16,15 @@ contract ERC20Token is ERC20 {
     
     constructor(uint256 initialSupply) ERC20("Lady", "LDY") {
         _mint(msg.sender, initialSupply);
+        admin = msg.sender; 
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, 'only admin');
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        
     }
 }
